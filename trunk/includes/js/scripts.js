22160853e6cc8902b570a31479bc9c7cc6a92e8b
Ivan Petermann <contato@ivanpetermann.com>
2021-08-27 09:49:58 -0300

    Version 1.7 - Added new mask formats. (start, time2, zip, taxid, ssn, money3, money4, percent2, percent3, percent4)

--- trunk/includes/js/scripts.js
+++ trunk/includes/js/scripts.js
@@ -1,34 +1,44 @@
 (function($) {
 
-	'use strict';
+    'use strict';
 
-	if (typeof _mff == 'undefined' || _mff === null) {
-		return;
-	}
+    if (typeof _mff == 'undefined' || _mff === null) {
+        return;
+    }
 
-	jQuery(document).ready(function($){
-		$('span.date, input.date').mask('00/00/0000');
-		$('span.time, input.time').mask('00:00:00');
-		$('input.date_time').mask('00/00/0000 00:00:00');
-		$('input.cep').mask('00000-000');
-		$('input.phone').mask(PhoneMaskBehavior, nonoOptions);
-		$('input.phone_us').mask('(000) 000-0000');
-		$('input.mixed').mask('AAA 000-S0S');
-		$('input.cpf').mask('000.000.000-00', {reverse: true});
-		$('input.cnpj').mask('00.000.000/0000-00', {reverse: true});
-		$('input.money').mask('000.000.000.000.000,00', {reverse: true});
-		$('input.money2').mask("#.##0,00", {reverse: true});
-		$('input.ip_address').mask('099.099.099.099');
-		$('input.percent').mask('##0,00%', {reverse: true});
-	});
+    jQuery(document).ready(function($) {
+        $('span.start, input.start').mask('00/0000');
+        $('span.date, input.date').mask('00/00/0000');
+        $('span.time, input.time').mask('00:00:00');
+        $('span.time2, input.time2').mask('00:00');
+        $('input.date_time').mask('00/00/0000 00:00:00');
+        $('input.cep').mask('00000-000');
+        $('input.zip').mask('00000');
+        $('input.phone').mask(PhoneMaskBehavior, nonoOptions);
+        $('input.phone_us').mask('(000) 000-0000');
+        $('input.taxid').mask('00-0000000');
+        $('input.ssn').mask('000-00-0000');
+        $('input.mixed').mask('AAA 000-S0S');
+        $('input.cpf').mask('000.000.000-00', { reverse: true });
+        $('input.cnpj').mask('00.000.000/0000-00', { reverse: true });
+        $('input.money').mask('000.000.000.000.000,00', { reverse: true });
+        $('input.money2').mask('#.##0,00', { reverse: true });
+        $('input.money3').mask('#,##0.00', { reverse: true });
+        $('input.money4').mask('000,000,000,000,000.00', { reverse: true });
+        $('input.ip_address').mask('099.099.099.099');
+        $('input.percent').mask('##0,00%', { reverse: true });
+        $('input.percent2').mask('##0.00%', { reverse: true });
+        $('input.percent3').mask('990,0%', { reverse: true });
+        $('input.percent4').mask('990.0%', { reverse: true });
+    });
 
-	var PhoneMaskBehavior = function (val) {
-		return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-	},
-	nonoOptions = {
-	  onKeyPress: function(val, e, field, options) {
-	      field.mask(PhoneMaskBehavior.apply({}, arguments), options);
-	    }
-	};
+    var PhoneMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+        },
+        nonoOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(PhoneMaskBehavior.apply({}, arguments), options);
+            }
+        };
 
 })(jQuery);
--- trunk/readme.txt
+++ trunk/readme.txt
@@ -5,7 +5,7 @@ Tags: email, mask, form, input, field, phone, masks, fields, petermann
 Requires at least: 3.9
 Tested up to: 5.8
 Requires PHP: 5.6
-Stable tag: 1.6
+Stable tag: 1.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -17,19 +17,31 @@ Basic Usage Examples
 
 Add a class in the form field.
 
-* ('span.date, input.date').mask('00/00/0000');
-* ('span.time, input.time').mask('00:00:00');
-* ('input.date_time').mask('00/00/0000 00:00:00');
-* ('input.cep').mask('00000-000');
-* ('input.phone').mask('(00) 0000-00009');
-* ('input.phone_us').mask('(000) 000-0000');
-* ('input.mixed').mask('AAA 000-S0S');
-* ('input.cpf').mask('000.000.000-00');
-* ('input.cnpj').mask('00.000.000/0000-00');
-* ('input.money').mask('000.000.000.000.000,00');
-* ('input.money2').mask('#.##0,00');
-* ('input.ip_address').mask('099.099.099.099');
-* ('input.percent').mask('##0,00%');
+`('span.start, input.start').mask('00/0000');
+('span.date, input.date').mask('00/00/0000');
+('span.time, input.time').mask('00:00:00');
+('span.time2, input.time2').mask('00:00');
+('input.date_time').mask('00/00/0000 00:00:00');
+('input.cep').mask('00000-000');
+('input.zip').mask('00000');
+('input.phone').mask(PhoneMaskBehavior, nonoOptions);
+('input.phone_us').mask('(000) 000-0000');
+('input.taxid').mask('00-0000000');
+('input.ssn').mask('000-00-0000');
+('input.mixed').mask('AAA 000-S0S');
+('input.cpf').mask('000.000.000-00', { reverse: true });
+('input.cnpj').mask('00.000.000/0000-00', { reverse: true });
+('input.money').mask('000.000.000.000.000,00', { reverse: true });
+('input.money2').mask('#.##0,00', { reverse: true });
+('input.money3').mask('#,##0.00', { reverse: true });
+('input.money4').mask('000,000,000,000,000.00', { reverse: true });
+('input.ip_address').mask('099.099.099.099');
+('input.percent').mask('##0,00%', { reverse: true });
+('input.percent2').mask('##0.00%', { reverse: true });
+('input.percent3').mask('990,0%', { reverse: true });
+('input.percent4').mask('990.0%', { reverse: true });`
+
+[Click here](https://gist.github.com/petermann/fd1a898e02ca91a0d7231a9f8ee662b4) for more details on how to add custom mask.
 
 == Installation ==
 
@@ -46,7 +58,14 @@ Installing "Masks Form Fields" can be done either by searching for "Masks Form F
 
 This plugin to add masks in the form’s fields.
 
+= How to add custom mask? =
+
+[Click here](https://gist.github.com/petermann/fd1a898e02ca91a0d7231a9f8ee662b4) for more details on how to add custom mask.
+
 == Changelog ==
+
+= 1.7 =
+* Added new mask formats. (start, time2, zip, taxid, ssn, money3, money4, percent2, percent3, percent4)
 
 = 1.6 =
 * Tested in WordPress 5.8
@@ -79,9 +98,8 @@ This plugin to add masks in the form’s fields.
 
 == Upgrade Notice ==
 
-= 1.5 =
-* Fixed the incorrect value in the localize script.
-* Tested in WordPress 5.7
+= 1.7 =
+* Added new mask formats.
 
 == Credits ==
 
