35f2883fd1fb8d3f19c1bf0b36a3dc1f58b14f45
Ivan Petermann <contato@ivanpetermann.com>
2023-08-20 11:14:21 -0300

    Tested up to 6.3

--- trunk/masks-form-fields.php
+++ trunk/masks-form-fields.php
@@ -7,7 +7,7 @@
  * Author: Ivan Petermann
  * Author URI: https://ivanpetermann.com
  * Requires at least: 3.9
- * Tested up to: 6.2
+ * Tested up to: 6.3
  *
  * Text Domain: masks-form-fields
  * Domain Path: /languages/
